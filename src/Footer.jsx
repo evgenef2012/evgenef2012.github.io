ebb4200238056b4072b35f985b7e79d3c2893a6d
evgenef2012 <evge1983@gmail.com>
2024-07-29 14:53:36 +0300

    Update Test 01

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Nefedov Evgeni</p>
+        </footer>
+    );
+}
+
+export default Footer
+
---
+++ src/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.css'
+
+
+function Header(){
+
+    return(
+        <header>
+            <h1>NEFEDOV</h1>
+            <hr></hr>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>Architecture</li>
+                    <li><a href="#"></a>Medical</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
